e556e1c9b135a3be2f44eaa9719409176648f1e8
dsjramesh7 <rameshdussa07@gmail.com>
2025-06-14 16:56:43 +0530

    ss2: databinding => interpolation

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
 
-  constructor() { }
+  // my code here
+  public interpolationLearningSomething: string =
+    'luffy pirate swordman deception';
 
-  ngOnInit(): void {
+  public todayDate(): string {
+    return new Date().toLocaleDateString();
   }
-
+  public nowTime(): string {
+    return new Date().toLocaleTimeString();
+  }
 }
