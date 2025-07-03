2ca22f8b2d7a19526c3362c60e55101ffa4e3457
dsjramesh7 <rameshdussa07@gmail.com>
2025-07-03 23:12:23 +0530

    auth-user

--- src/app/components/auth-user/auth-user.component.ts
+++ src/app/components/auth-user/auth-user.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-auth-user',
   templateUrl: './auth-user.component.html',
-  styleUrls: ['./auth-user.component.css']
+  styleUrls: ['./auth-user.component.css'],
 })
 export class AuthUserComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  //code here
+  public isLoggedIn: boolean = false;
+
+  public loginUser(): void {
+    this.isLoggedIn = true;
   }
 
+  public logOutUser(): void {
+    this.isLoggedIn = false;
+  }
 }
