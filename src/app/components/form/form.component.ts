72f9a246b4f8e75341a2c064ba3274c66b21486d
dsjramesh7 <rameshdussa07@gmail.com>
2025-07-03 19:44:58 +0530

    manual way

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  // all code here
+  public userName:string = ""
+
+  public updateName(e:any):void{
+    this.userName = e.target.value;
   }
 
 }
